b5f9fae33dd562a127985c9a9c2fd5f11998d47c
damianhijnen <damianhijnen@hotmail.com>
2025-01-22 13:27:45 +0100

    Combat looping werkt Combat aanvallen werkt
    Stamina en mana consumption basis
    attack class aangemaat

--- core/combat.py
+++ core/combat.py
@@ -1,9 +1,11 @@
 import random
+import re
 from core.enemies.enemies import Enemy
+from core.entities import attack
 # testSession = testData.constructSession()
 # Nodig voor main mischien verplaatsen??
 
-attacks = [["stab", 1], ["slash", 2]]
+attacks = [attack("stab", 1, 0, 0), attack("slash", 2, 1, 0)]
 
 def combat(inp, session):
     character = session.character
@@ -12,18 +14,31 @@ def combat(inp, session):
     while True:
         for enemy in enemies:
             print(enemy.name + " " +  str(enemy.hp))
-        inp = input("Action: ")
+        inp = input("Action \n 1. Attack \n Choose action: ")
         if inp == "end":
             print("you ended the senario")
             break
         # start attack
         elif inp == "1":
-            print("attack")
+            print("Your attacking \n")
+            # Attack player fase
             while True:
+                # Choosing attack
                 for attack in attacks:
-                    print(attack[0] + " " + str(attack[1]))
-                inp = input("Attack: ")
-
+                    print(attack.name + " dmg: " + str(attack.damage))
+                inp = int(re.sub("[^0-9]", "", input("Choose Attack: ")))
+                if inp != "":
+                    att = attacks[inp-1]
+                    # Selecting enemy
+                    for index, enemy in enumerate(enemies):
+                        print(str(index+1) + " " + enemy.name + " " +  str(enemy.hp))
+                    inp = int(re.sub("[^0-9]", "", input("\nChoose enemy: ")))
+                    if inp >= 1 and inp <= len(enemies):
+                        inp -= 1
+                        print("Attacking " + enemies[inp].name)
+                        enemies[inp].hp = enemies[inp].hp - att.damage
+                        print("You " + att.name + " " + enemies[inp].name + " for " + str(att.damage) + " dmg")
+                        break
     session.state = "travel"
     return session
 
--- core/entities.py
+++ core/entities.py
@@ -1,12 +1,23 @@
 #Rogue, Wizzard
 class Character:
-    def __init__(self, strength: int = 1, accuracy: int = 1, speed: int = 1, endurance: int = 1, hp: int = 100):
+    def __init__(self, strength: int = 1, accuracy: int = 1, speed: int = 1, endurance: int = 1, stamina: int = 100, mana: int = 100, hp: int = 100):
         self.strength = strength
         self.accuracy = accuracy
         self.speed = speed
         self.endurance = endurance
+        self.stamina = stamina
+        self.mana = mana
         self.hp = hp
 
 
 
+
+
+class attack:
+    def __init__(self, name: str = "Error NULL", damage: int = 1, stamina: int = 0, mana: int = 0, cooldown: int = 0):
+        self.name = name
+        self.damage = damage
+        self.stamina = stamina
+        self.mana = mana
+        self.cooldown = cooldown
 
